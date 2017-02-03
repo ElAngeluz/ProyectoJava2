4987ae552c93b853e58a86434da90608a8f5a80c
Janpii <parralesjael@gmail.com>
2017-02-03 11:36:25 -0500

    new

---
+++ new/src/Entidades/Cliente.java
@@ -0,0 +1,102 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package Entidades;
+
+/**
+ *
+ * @author Johanna Parrales
+ */
+public class Cliente {
+    private int id;
+    private String nombre;
+    private String apellido;
+    private String direccion;
+    private String telefono;
+
+    public Cliente(int id, String nombre, String apellido, String direccion, String telefono) {
+        this.id = id;
+        this.nombre = nombre;
+        this.apellido = apellido;
+        this.direccion = direccion;
+        this.telefono = telefono;
+    }
+
+    public Cliente(int id) {
+        this.id = id;
+    }
+
+     /**
+     * @return the id
+     */
+    public int getId() {
+        return id;
+    }
+
+    /**
+     * @param id the id to set
+     */
+    public void setId(int id) {
+        this.id = id;
+    }
+
+    /**
+     * @return the nombre
+     */
+    public String getNombre() {
+        return nombre;
+    }
+
+    /**
+     * @param nombre the nombre to set
+     */
+    public void setNombre(String nombre) {
+        this.nombre = nombre;
+    }
+
+    /**
+     * @return the apellido
+     */
+    public String getApellido() {
+        return apellido;
+    }
+
+    /**
+     * @param apellido the apellido to set
+     */
+    public void setApellido(String apellido) {
+        this.apellido = apellido;
+    }
+
+    /**
+     * @return the direccion
+     */
+    public String getDireccion() {
+        return direccion;
+    }
+
+    /**
+     * @param direccion the direccion to set
+     */
+    public void setDireccion(String direccion) {
+        this.direccion = direccion;
+    }
+
+    /**
+     * @return the telefono
+     */
+    public String getTelefono() {
+        return telefono;
+    }
+
+    /**
+     * @param telefono the telefono to set
+     */
+    public void setTelefono(String telefono) {
+        this.telefono = telefono;
+    }
+    
+    
+}
---
+++ new/src/Entidades/Productos.java
@@ -0,0 +1,72 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package Entidades;
+
+import java.sql.Date;
+
+/**
+ *
+ * @author parivera
+ */
+public class Productos {
+    private int codigo;
+    private String nombre;
+    private String marca;
+    private Date fecha_Caducidad;
+    private int cantidad;
+    
+    public Productos(int _codigo, String _nombre, String _marca, Date _fechaCadu, int _cantidad){
+        codigo = _codigo;
+        nombre = _nombre;
+        marca = _marca;
+        fecha_Caducidad = _fechaCadu;
+        cantidad = _cantidad;
+    }
+
+    public Productos(int _codigo) {
+        codigo = _codigo;
+    }
+
+    public int getCodigo() {
+        return codigo;
+    }
+
+    public void setCodigo(int codigo) {
+        this.codigo = codigo;
+    }
+
+    public String getNombre() {
+        return nombre;
+    }
+
+    public void setNombre(String nombre) {
+        this.nombre = nombre;
+    }
+
+    public String getMarca() {
+        return marca;
+    }
+
+    public void setMarca(String marca) {
+        this.marca = marca;
+    }
+
+    public Date getFecha_Caducidad() {
+        return fecha_Caducidad;
+    }
+
+    public void setFecha_Caducidad(Date fecha_Caducidad) {
+        this.fecha_Caducidad = fecha_Caducidad;
+    }
+
+    public int getCantidad() {
+        return cantidad;
+    }
+
+    public void setCantidad(int cantidad) {
+        this.cantidad = cantidad;
+    }
+}
